92123e389352dd59f239cd35ff11aec6135129ce
t_aleksandr <b.arvinok1977@gmail.com>
2017-06-04 16:35:57 -0700

    Modified App class

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	 System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+    	System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
     }
 }
